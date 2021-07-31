8eb911f10f51a2958f8c23dd1fe1ce6b325a05c2
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-07-31 18:43:31 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats=['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function appendCat(name) {
+    const newCats=[...cats, 'Broom'];
+    return newCats;
+}
+
+function prependCat(name) {
+    const reallyNewCats=['Arnold',...cats];
+    return reallyNewCats;
+}
+
+function removeLastCat() {
+    const oldCats=cats.slice(0,-1);
+    return oldCats;
+}
+
+function removeFirstCat() {
+    const doneCats=cats.slice(1);
+    return doneCats;
+}
